4b2c9cf9cfcb3fecdcce24c526fa48fb0a8eac56
lucasDSBR <lucasmaciel6690@gmail.com>
2025-01-16 10:05:29 -0300

    updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (request()->getHost() === 'sospragas.pt') {
+			\URL::forceRootUrl('https://sospragas.pt');
+		}
     }
 }
