252d961599ce2ff7f49f37a89b76bbecbfd2ed5d
lucasDSBR <lucasmaciel6690@gmail.com>
2025-04-29 12:02:46 -0300

    updates

--- resources/views/services/show.blade.php
+++ resources/views/services/show.blade.php
@@ -959,27 +959,50 @@ setTimeout(function() {
             });
         });
     </script>
+<script src="{{ asset('assets/vendors/jquery/jquery-3.7.0.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/bootstrap-select/bootstrap-select.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/jarallax/jarallax.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/jquery-ui/jquery-ui.js') }}"></script>
+<script src="{{ asset('assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/jquery-appear/jquery.appear.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/jquery-validate/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/nouislider/nouislider.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/tiny-slider/tiny-slider.js') }}"></script>
+<script src="{{ asset('assets/vendors/wnumb/wNumb.min.js') }}"></script>
+{{-- <script src="{{ asset('assets/vendors/owl-carousel/js/owl.carousel.min.js') }}"></script> --}}
+<script src="{{ asset('assets/vendors/wow/wow.js') }}"></script>
+<script src="{{ asset('assets/vendors/tilt/tilt.jquery.js') }}"></script>
+<script src="{{ asset('assets/vendors/simpleParallax/simpleParallax.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/imagesloaded/imagesloaded.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/isotope/isotope.js') }}"></script>
+<script src="{{ asset('assets/vendors/countdown/countdown.min.js') }}"></script>
+<script src="{{ asset('assets/vendors/jquery-circleType/jquery.circleType.js') }}"></script>
+{{-- <script src="{{ asset('assets/vendors/jquery-lettering/jquery.lettering.min.js') }}"></script> --}}
+{{-- <script src="{{ asset('assets/vendors/progress-bar/knob.js') }}"></script> --}}
 
-	<!-- chart js -->
-	<script src="{{ asset('assets/vendors/chart/chart.js') }}"></script>
-	<script src="{{ asset('assets/vendors/chart/custome-chart.js') }}"></script>
+<!-- chart js -->
+<script src="{{ asset('assets/vendors/chart/chart.js') }}"></script>
+<script src="{{ asset('assets/vendors/chart/custome-chart.js') }}"></script>
 
-	<!-- template js -->
-	<script src="{{ asset('assets/js/tolak.js') }}"></script>
+<!-- template js -->
+<script src="{{ asset('assets/js/tolak.js') }}"></script>
 
-	<!-- sanito -->
+<!-- sanito -->
 
-	<script src="{{ asset('assets/js/jquery.js') }}"></script>
-	<script src="{{ asset('assets/js/popper.min.js') }}"></script>
-	<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-	<script src="{{ asset('assets/js/owl.js') }}"></script>
-	<script src="{{ asset('assets/js/wow.js') }}"></script>
-	<script src="{{ asset('assets/js/validation.js') }}"></script>
-	<script src="{{ asset('assets/js/jquery.fancybox.js') }}"></script>
-	<script src="{{ asset('assets/js/appear.js') }}"></script>
-	<script src="{{ asset('assets/js/jquery.countTo.js') }}"></script>
-	<script src="{{ asset('assets/js/scrollbar.js') }}"></script>
-	<script src="{{ asset('assets/js/jquery.nice-select.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.js') }}"></script>
+<script src="{{ asset('assets/js/popper.min.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/owl.js') }}"></script>
+<script src="{{ asset('assets/js/wow.js') }}"></script>
+<script src="{{ asset('assets/js/validation.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.fancybox.js') }}"></script>
+<script src="{{ asset('assets/js/appear.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.countTo.js') }}"></script>
+<script src="{{ asset('assets/js/scrollbar.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.nice-select.min.js') }}"></script>
 	<script>
 		$(".form-contacts").on('submit', (e) => {
 		   // e.target é o formulário que disparou o evento
