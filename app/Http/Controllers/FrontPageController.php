3c279b2708389f235a264ed650d7c8affc970341
lucasDSBR <lucasmaciel6690@gmail.com>
2025-05-14 19:48:24 -0300

    updates

--- app/Http/Controllers/FrontPageController.php
+++ app/Http/Controllers/FrontPageController.php
@@ -604,6 +604,58 @@ class FrontPageController extends Controller
 
 		}
 
+		if($request->input('type_form') == 'ContactFormContactos'){
+			$secretKey = 'ES_8cf3c7f61ea84e6ebb4446cb84cfa8e4'; // Substitua pela sua chave secreta
+			$verifyUrl = 'https://api.hcaptcha.com/siteverify';
+
+			// Recebe o token da resposta enviada do formulário
+			$token = $_POST['h-captcha-response']?? '';
+
+			// Cria os dados para enviar na requisição POST
+			$data = [
+				'secret' => $secretKey,
+				'response' => $token
+			];
+
+			// Faz a requisição POST para a API do hCaptcha
+			$options = [
+				'http' => [
+					'method'  => 'POST',
+					'content' => http_build_query($data),
+					'header'  => "Content-Type: application/x-www-form-urlencoded\r\n"
+				]
+			];
+			$context  = stream_context_create($options);
+			$response = file_get_contents($verifyUrl, false, $context);
+
+			// Decodifica a resposta JSON
+			$responseJson = json_decode($response, true);
+
+			// Verifica se a verificação foi bem-sucedida
+			$success = $responseJson['success'];
+
+			if ($success) {
+				$query = contact_forms::create([
+					'name' => $request->input('name'),
+					'email' => $request->input('email'),
+					'phone' => $request->input('phone'),
+					'confirmed' => true
+				]);
+				if ($query) {
+					$contact_info = ContactInfo::where('id',1)->get()->toArray();
+					$main = SustainabilityPage::where('id', 1)->get()->toArray();
+					return view('thank-you.index', [
+						'contact_info' => $contact_info,
+						'main' => $main
+					]);
+				}
+				return redirect()->back()->with('error', 'Ocorreu um erro ao enviar o formulário. Tente novamente.');
+			} else {
+				return redirect()->back()->with('error', 'Ocorreu um erro ao enviar o formulário. Tente novamente.');
+			}
+
+		}
+
 		if($request->input('type_form') == 'ScheduleInspection'){
 			//dd($request->all());
 			$query = schedule_inspection::create([
