7e549f96f55d8562e929c97df71e0e143511767c
LucasMDEV5330 <lucasmaciel6690@gmail.com>
2025-07-11 08:47:59 -0300

    Updates SEO

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -247,21 +247,21 @@ setTimeout(function() {
     </style>
     <!-- jequery plugins -->
 
-    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}" defer></script>
     <script src="{{ asset('assets/js/jquery.js') }}" defer></script>
     <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 
-    <script src="{{ asset('assets/js/owl.js') }}"></script>
-    <script src="{{ asset('assets/js/wow.js') }}"></script>
-    <script src="{{ asset('assets/js/validation.js') }}"></script>
+    <script src="{{ asset('assets/js/owl.js') }}" defer></script>
+    <script src="{{ asset('assets/js/wow.js') }}" defer></script>
+    <script src="{{ asset('assets/js/validation.js') }}" defer></script>
     <script src="{{ asset('assets/js/jquery.fancybox.js') }}" defer ></script>
-    <script src="{{ asset('assets/js/appear.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.countTo.js') }}"></script>
-    <script src="{{ asset('assets/js/scrollbar.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('assets/js/appear.js') }}" defer></script>
+    <script src="{{ asset('assets/js/jquery.countTo.js') }}" defer></script>
+    <script src="{{ asset('assets/js/scrollbar.js') }}" defer></script>
+    <script src="{{ asset('assets/js/jquery.nice-select.min.js') }}" defer></script>
 
     <!-- main-js -->
-    <script src="{{ asset('assets/js/script.js') }}"></script>
+    <script src="{{ asset('assets/js/script.js') }}" defer></script>
 </head>
 
 <body class="custom-cursor" style="overflow-x: hidden;">
