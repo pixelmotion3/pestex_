c23f0a6b3ccab2f4668a62c55b4aaba4f8447228
LucasMDEV5330 <lucasmaciel6690@gmail.com>
2025-09-04 00:22:49 -0300

    updates

--- app/Http/Middleware/CheckCookieConsent.php
+++ app/Http/Middleware/CheckCookieConsent.php
@@ -10,7 +10,7 @@ class CheckCookieConsent
     public function handle(Request $request, Closure $next)
     {
         // Verifica se o cookie de consentimento já foi definido
-        if (!$request->hasCookie('cookie_consent')) {
+        if (!$request->hasCookie('cookie_consent_sosp')) {
             // Passa uma variável para as views indicando que o consentimento não foi dado
             view()->share('showCookieBanner', true);
         } else {
